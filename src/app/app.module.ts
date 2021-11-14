1c7520f49a2d84856f639a3ad2694527afefc50d
Ciubucciu Adrian Albert <ciubucciu.adrianalbert@gmail.com>
2021-11-15 00:38:17 +0200

    solving all the errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,72 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
+import { AppComponent } from './app.component';
+// import { UserProfileComponent } from './user-profile/user-profile.component';
+// import { IconsComponent } from './icons/icons.component';
+// import { MapsComponent } from './maps/maps.component';
+// import { NotificationsComponent } from './notifications/notifications.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule} from '@angular/material/table';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select'
+import { MatGridListModule } from '@angular/material/grid-list';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+// import { MatTableDataSource } from '@angular/material/table'
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { WalletsComponent } from './pages/wallets/wallets.component';
-
-
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     ComponentsModule,
-    NgbModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatTableModule,
+    MatTabsModule,
+    MatExpansionModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatPaginatorModule,
+    MatSortModule,
+    // MatTableDataSource,
+    NgbModule
   ],
   declarations: [
     AppComponent,
